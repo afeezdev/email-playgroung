e3be70df3cfbb2e2584fa134ba9476de6e3f9540
afeezdev <afeezdev@gmail.com>
2023-10-15 00:56:51 +0100

    email sender with python3

--- email_send.py
+++ email_send.py
@@ -1,17 +1,20 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'Afeez Abdulyekeen'
+email['from'] = 'Olawale Python'
 email['to'] = 'afeezdev@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars!'
 
-email.set_content('I am a Python Master')
+email.set_content(html.substitute({'name':'TinTin' , 'age':34}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('afeezdev@gmail.com', '************')
+    smtp.login('afeezdev@gmail.com', '*********')
     smtp.send_message(email)
     print('all good Boss!')
 
