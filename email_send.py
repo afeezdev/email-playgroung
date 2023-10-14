bc6ecc961246f106a644dbf7a013752d62edddc2
afeezdev <afeezdev@gmail.com>
2023-10-15 00:36:44 +0100

    email sender app

---
+++ email_send.py
@@ -0,0 +1,17 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Afeez Abdulyekeen'
+email['to'] = 'afeezdev@gmail.com'
+email['subject'] = 'You won 1,000,000 dollars!'
+
+email.set_content('I am a Python Master')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('afeezdev@gmail.com', '*************')
+    smtp.send_message(email)
+    print('all good Boss!')
+
